653a7ca0808871e98cadc89919fcc45bd98ef06c
xuanhanh.dev <xuanhanh.setdy@gmail.com>
2025-08-04 11:42:05 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -209,15 +209,6 @@
                 <div class="block block-rounded flex-grow-1 d-flex flex-column">
                     <div class="block-header block-header-default">
                         <h3 class="block-title">Thống kê trong tuần</h3>
-                        <div class="block-options">
-                            <button type="button" class="btn-block-option" data-toggle="block-option"
-                                    data-action="state_toggle" data-action-mode="demo">
-                                <i class="si si-refresh"></i>
-                            </button>
-                            <button type="button" class="btn-block-option">
-                                <i class="si si-settings"></i>
-                            </button>
-                        </div>
                     </div>
                     <div class="block-content block-content-full flex-grow-1 d-flex align-items-center">
                         <!-- Earnings Chart Container -->
